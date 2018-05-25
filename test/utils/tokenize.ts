091093a8aef896f3d61a8defa6078f122ed6920a
Nathan Totten <ntotten@salesforce.com>
2018-05-25 12:32:43 -0400

    Updated license info

--- test/utils/tokenize.ts
+++ test/utils/tokenize.ts
@@ -1,6 +1,7 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Modifications Copyright (c) 2018 Salesforce.
+ *  See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
 import { ITokenizeLineResult, Registry, StackElement } from 'vscode-textmate';
