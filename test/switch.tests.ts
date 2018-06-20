9a516f89ddcbfcb58f11950218aafb0c564c0fc1
Luis Campos <lcamposguajardo@salesforce.com>
2018-06-19 17:15:45 -0700

    Add tokens and commit first working test and set of rules

--- test/switch.tests.ts
+++ test/switch.tests.ts
@@ -9,11 +9,12 @@ describe('Grammar', () => {
   describe('Switch Statements', () => {
       it('simple switch', () => {
         const input = Input.InMethod(`
-switch on(param) {
+switch on (param) {
   when 'A' {
     System.debug('test');
   }
   when else {
+    callExternalMethod();
   }
 }`);
         const tokens = tokenize(input);
@@ -25,47 +26,30 @@ switch on(param) {
           Token.Variables.ReadWrite('param'),
           Token.Punctuation.CloseParen,
           Token.Punctuation.OpenBrace,
-
-          /*Token.Keywords.Switch.When,
+          Token.Keywords.Switch.When,
           Token.Punctuation.String.Begin,
           Token.Literals.String('A'),
           Token.Punctuation.String.End,
           Token.Punctuation.OpenBrace,
-          Token.Comment.LeadingWhitespace('    '),
-          Token.Comment.SingleLine.Start,
-          Token.Comment.SingleLine.Text(' some comment.'),
+          Token.Support.Class.System,
+          Token.Punctuation.Accessor,
+          Token.Support.Class.FunctionText('debug'),
+          Token.Punctuation.OpenParen,
+          Token.XmlDocComments.String.SingleQuoted.Begin,
+          Token.XmlDocComments.String.SingleQuoted.Text('test'),
+          Token.XmlDocComments.String.SingleQuoted.End,
+          Token.Punctuation.CloseParen,
+          Token.Punctuation.Semicolon,
           Token.Punctuation.CloseBrace,
-
           Token.Keywords.Switch.When,
-          Token.Punctuation.String.Begin,
-          Token.Literals.String('B'),
-          Token.Punctuation.String.End,
+          Token.Keywords.Switch.Else,
           Token.Punctuation.OpenBrace,
-          Token.Support.Class.System,
-          Token.Punctuation.Accessor,
-          Token.Support.Class.FunctionText('debug'),
+          Token.Identifiers.MethodName('callExternalMethod'),
           Token.Punctuation.OpenParen,
-          Token.XmlDocComments.String.SingleQuoted.Begin,
-          Token.XmlDocComments.String.SingleQuoted.Text('test'),
-          Token.XmlDocComments.String.SingleQuoted.End,
           Token.Punctuation.CloseParen,
           Token.Punctuation.Semicolon,
-          Token.Punctuation.CloseBrace,*/
-
-          // when else
-          /*Token.Punctuation.OpenBrace,
-          Token.Support.Class.System,
-          Token.Punctuation.Accessor,
-          Token.Support.Class.FunctionText('debug'),
-          Token.Punctuation.OpenParen,
-          Token.XmlDocComments.String.SingleQuoted.Begin,
-          Token.XmlDocComments.String.SingleQuoted.Text('test'),
-          Token.XmlDocComments.String.SingleQuoted.End,
-          Token.Punctuation.CloseParen,
-          Token.Punctuation.Semicolon,
-          Token.Punctuation.CloseBrace,*/
-
-          //Token.Punctuation.CloseBrace
+          Token.Punctuation.CloseBrace,
+          Token.Punctuation.CloseBrace
         ]);
       });
   });
--- test/utils/tokenize.ts
+++ test/utils/tokenize.ts
@@ -339,6 +339,7 @@ export namespace Token {
       );
       export const On = createToken('on', 'keyword.control.switch.on.apex');
       export const When = createToken('when', 'keyword.control.switch.when.apex');
+      export const Else = createToken('else', 'keyword.control.switch.else.apex');
     }
 
     export namespace Queries {
