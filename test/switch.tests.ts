dd9a28892e38d1cff6bf7947288dc7245740472b
Luis Campos <lcamposguajardo@salesforce.com>
2018-06-20 12:03:54 -0700

    Support different values, expressions, etc

--- test/switch.tests.ts
+++ test/switch.tests.ts
@@ -7,50 +7,173 @@ describe('Grammar', () => {
   });
 
   describe('Switch Statements', () => {
-      it('simple switch', () => {
-        const input = Input.InMethod(`
+    it('simple switch', () => {
+      const input = Input.InMethod(`
 switch on (param) {
-  when 'A' {
-    System.debug('test');
-  }
-  when else {
-    callExternalMethod();
-  }
+when 'A' {
+  System.debug('test');
+}
+when else {
+  callExternalMethod();
+}
 }`);
-        const tokens = tokenize(input);
+      const tokens = tokenize(input);
 
-        tokens.should.deep.equal([
-          Token.Keywords.Switch.Switch,
-          Token.Keywords.Switch.On,
-          Token.Punctuation.OpenParen,
-          Token.Variables.ReadWrite('param'),
-          Token.Punctuation.CloseParen,
-          Token.Punctuation.OpenBrace,
-          Token.Keywords.Switch.When,
-          Token.Punctuation.String.Begin,
-          Token.Literals.String('A'),
-          Token.Punctuation.String.End,
-          Token.Punctuation.OpenBrace,
-          Token.Support.Class.System,
-          Token.Punctuation.Accessor,
-          Token.Support.Class.FunctionText('debug'),
-          Token.Punctuation.OpenParen,
-          Token.XmlDocComments.String.SingleQuoted.Begin,
-          Token.XmlDocComments.String.SingleQuoted.Text('test'),
-          Token.XmlDocComments.String.SingleQuoted.End,
-          Token.Punctuation.CloseParen,
-          Token.Punctuation.Semicolon,
-          Token.Punctuation.CloseBrace,
-          Token.Keywords.Switch.When,
-          Token.Keywords.Switch.Else,
-          Token.Punctuation.OpenBrace,
-          Token.Identifiers.MethodName('callExternalMethod'),
-          Token.Punctuation.OpenParen,
-          Token.Punctuation.CloseParen,
-          Token.Punctuation.Semicolon,
-          Token.Punctuation.CloseBrace,
-          Token.Punctuation.CloseBrace
-        ]);
-      });
+      tokens.should.deep.equal([
+        Token.Keywords.Switch.Switch,
+        Token.Keywords.Switch.On,
+        Token.Punctuation.OpenParen,
+        Token.Variables.ReadWrite('param'),
+        Token.Punctuation.CloseParen,
+        Token.Punctuation.OpenBrace,
+        Token.Keywords.Switch.When,
+        Token.Punctuation.String.Begin,
+        Token.Literals.String('A'),
+        Token.Punctuation.String.End,
+        Token.Punctuation.OpenBrace,
+        Token.Support.Class.System,
+        Token.Punctuation.Accessor,
+        Token.Support.Class.FunctionText('debug'),
+        Token.Punctuation.OpenParen,
+        Token.XmlDocComments.String.SingleQuoted.Begin,
+        Token.XmlDocComments.String.SingleQuoted.Text('test'),
+        Token.XmlDocComments.String.SingleQuoted.End,
+        Token.Punctuation.CloseParen,
+        Token.Punctuation.Semicolon,
+        Token.Punctuation.CloseBrace,
+        Token.Keywords.Switch.When,
+        Token.Keywords.Switch.Else,
+        Token.Punctuation.OpenBrace,
+        Token.Identifiers.MethodName('callExternalMethod'),
+        Token.Punctuation.OpenParen,
+        Token.Punctuation.CloseParen,
+        Token.Punctuation.Semicolon,
+        Token.Punctuation.CloseBrace,
+        Token.Punctuation.CloseBrace
+      ]);
+    });
+
+    it('simple - single value', () => {
+      const input = Input.InMethod(`
+        switch on i {
+           when 2 {
+               System.debug('when block 2');
+           }
+           when -3 {
+               System.debug('when block -3');
+           }
+           when else {
+             // some comment.
+           }
+        }`);
+      const tokens = tokenize(input);
+
+      tokens.should.deep.equal([
+        Token.Keywords.Switch.Switch,
+        Token.Keywords.Switch.On,
+        Token.Variables.ReadWrite('i'),
+        Token.Punctuation.OpenBrace,
+        Token.Keywords.Switch.When,
+        Token.Literals.Numeric.Decimal('2'),
+        Token.Punctuation.OpenBrace,
+        Token.Support.Class.System,
+        Token.Punctuation.Accessor,
+        Token.Support.Class.FunctionText('debug'),
+        Token.Punctuation.OpenParen,
+        Token.XmlDocComments.String.SingleQuoted.Begin,
+        Token.XmlDocComments.String.SingleQuoted.Text('when block 2'),
+        Token.XmlDocComments.String.SingleQuoted.End,
+        Token.Punctuation.CloseParen,
+        Token.Punctuation.Semicolon,
+        Token.Punctuation.CloseBrace,
+        Token.Keywords.Switch.When,
+        Token.Operators.Arithmetic.Subtraction,
+        Token.Literals.Numeric.Decimal('3'),
+        Token.Punctuation.OpenBrace,
+        Token.Support.Class.System,
+        Token.Punctuation.Accessor,
+        Token.Support.Class.FunctionText('debug'),
+        Token.Punctuation.OpenParen,
+        Token.XmlDocComments.String.SingleQuoted.Begin,
+        Token.XmlDocComments.String.SingleQuoted.Text('when block -3'),
+        Token.XmlDocComments.String.SingleQuoted.End,
+        Token.Punctuation.CloseParen,
+        Token.Punctuation.Semicolon,
+        Token.Punctuation.CloseBrace,
+        Token.Keywords.Switch.When,
+        Token.Keywords.Switch.Else,
+        Token.Punctuation.OpenBrace,
+        Token.Comment.LeadingWhitespace('             '),
+        Token.Comment.SingleLine.Start,
+        Token.Comment.SingleLine.Text(' some comment.'),
+        Token.Punctuation.CloseBrace,
+        Token.Punctuation.CloseBrace
+      ]);
+    });
+
+    it('simple - null value', () => {
+      const input = Input.InMethod(`
+        switch on someValue {
+   when 2 {
+       System.debug('when block 2');
+   }
+   when null {
+       System.debug('bad integer');
+   }
+   when else {
+       System.debug('default ' + someValue);
+   }
+}`);
+      const tokens = tokenize(input);
+
+      tokens.should.deep.equal([
+        Token.Keywords.Switch.Switch,
+        Token.Keywords.Switch.On,
+        Token.Variables.ReadWrite('someValue'),
+        Token.Punctuation.OpenBrace,
+        Token.Keywords.Switch.When,
+        Token.Literals.Numeric.Decimal('2'),
+        Token.Punctuation.OpenBrace,
+        Token.Support.Class.System,
+        Token.Punctuation.Accessor,
+        Token.Support.Class.FunctionText('debug'),
+        Token.Punctuation.OpenParen,
+        Token.XmlDocComments.String.SingleQuoted.Begin,
+        Token.XmlDocComments.String.SingleQuoted.Text('when block 2'),
+        Token.XmlDocComments.String.SingleQuoted.End,
+        Token.Punctuation.CloseParen,
+        Token.Punctuation.Semicolon,
+        Token.Punctuation.CloseBrace,
+        Token.Keywords.Switch.When,
+        Token.Literals.Null,
+        Token.Punctuation.OpenBrace,
+        Token.Support.Class.System,
+        Token.Punctuation.Accessor,
+        Token.Support.Class.FunctionText('debug'),
+        Token.Punctuation.OpenParen,
+        Token.XmlDocComments.String.SingleQuoted.Begin,
+        Token.XmlDocComments.String.SingleQuoted.Text('bad integer'),
+        Token.XmlDocComments.String.SingleQuoted.End,
+        Token.Punctuation.CloseParen,
+        Token.Punctuation.Semicolon,
+        Token.Punctuation.CloseBrace,
+        Token.Keywords.Switch.When,
+        Token.Keywords.Switch.Else,
+        Token.Punctuation.OpenBrace,
+        Token.Support.Class.System,
+        Token.Punctuation.Accessor,
+        Token.Support.Class.FunctionText('debug'),
+        Token.Punctuation.OpenParen,
+        Token.XmlDocComments.String.SingleQuoted.Begin,
+        Token.XmlDocComments.String.SingleQuoted.Text('default '),
+        Token.XmlDocComments.String.SingleQuoted.End,
+        Token.Operators.Arithmetic.Addition,
+        Token.Variables.ReadWrite('someValue'),
+        Token.Punctuation.CloseParen,
+        Token.Punctuation.Semicolon,
+        Token.Punctuation.CloseBrace,
+        Token.Punctuation.CloseBrace
+      ]);
+    });
   });
 });
